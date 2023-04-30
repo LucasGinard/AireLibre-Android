78ecc4dd50b0fcf65ee202f65e19f7e310bc860b
LucasGinard <contactolucasginard@gmail.com>
2023-04-29 21:28:06 -0400

    go to app if press notification

--- app/src/main/java/com/lucasginard/airelibre/modules/notifications/NotificationManager.kt
+++ app/src/main/java/com/lucasginard/airelibre/modules/notifications/NotificationManager.kt
@@ -3,10 +3,13 @@ package com.lucasginard.airelibre.modules.notifications
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import com.lucasginard.airelibre.R
+import com.lucasginard.airelibre.modules.home.view.MainActivity
 import com.lucasginard.airelibre.utils.Constants
 
 class NotificationManager(private val context: Context) {
@@ -21,12 +24,16 @@ class NotificationManager(private val context: Context) {
             val channel = NotificationChannel(idSensorNotifiy, channelName, importance)
             notificationManager.createNotificationChannel(channel)
         }
+        val mainIntent = Intent(context, MainActivity::class.java)
+        mainIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        val pendingIntent = PendingIntent.getActivity(context, 0, mainIntent, PendingIntent.FLAG_MUTABLE)
 
         // Crear la notificación
         val builder = NotificationCompat.Builder(context, idSensorNotifiy)
             .setSmallIcon(R.mipmap.ic_launcher_round)
             .setContentTitle(title)
             .setContentText(description)
+            .setContentIntent(pendingIntent)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
 
         return  builder.build()
